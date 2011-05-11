e923e6bd7aa30966445b5981dbd25941523c107b
Mark Cavage <mark.cavage@joyent.com>
2011-05-11 13:03:08 -0700

    refactoring to use whiskey for tests

--- relay/tst/checks.test.js
+++ relay/tst/checks.test.js
@@ -1,172 +1,168 @@
 // Copyright 2011 Joyent, Inc.  All rights reserved.
 
 var http = require('httpu');
-
-var testCase = require('nodeunit').testCase;
 var restify = require('restify');
 var uuid = require('node-uuid');
 
 var App = require('../lib/app');
 var common = require('./lib/common');
 
-restify.log.level(restify.LogLevel.Warn);
+restify.log.level(restify.LogLevel.Debug);
 
-module.exports = testCase({
+// Globals for tests
+var app;
+var owner;
+var root;
+var socket;
+var zone;
 
-  setUp: function(callback) {
-    var self = this;
+function _options(path) {
+  var options = {
+    socketPath: socket,
+    method: 'POST',
+    path: '/checks/' + uuid(),
+    headers: {}
+  };
+  if (path) options.path += path;
+  options.headers['Content-Type'] = 'application/json';
+  options.headers['x-api-version'] = '6.1.0';
+  return options;
+}
 
-    this.socket = '/tmp/.' + uuid();
-    this.options = {
-      socketPath: self.socket,
-      method: 'POST',
-      path: '/checks/' + uuid(),
-      headers: {}
-    };
-    this.options.headers['x-api-version'] = '6.1.0';
+//// Tests
 
-    this.app = new App({
-      zone: uuid(),
-      path: self.socket,
-      owner: uuid(),
-      localMode: true,
-      configRoot: 'foo'
+exports.setUp = function(test, assert) {
+  var self = this;
+
+  socket = '/tmp/.' + uuid();
+  owner = uuid();
+  zone = uuid();
+  root = uuid();
+
+  app = new App({
+    zone: uuid(),
+    socket: socket,
+    owner: uuid(),
+    localMode: true,
+    configRoot: 'foo'
+  });
+  assert.ok(app);
+
+  app.listen(function() {
+    test.finish();
+  });
+};
+
+exports.test_missing_tatus = function(test, assert) {
+  http.request(_options(), function(res) {
+    common.checkResponse(assert, res);
+    assert.equal(res.statusCode, 409);
+    common.checkContent(assert, res, function() {
+      assert.ok(res.params);
+      assert.ok(res.params.code);
+      assert.ok(res.params.message);
+      assert.equal(res.params.code, 'MissingParameter');
+      test.finish();
     });
+  }).end();
+};
 
-    this.app.listen(callback);
-  },
-
-  tearDown: function(callback) {
-    this.app.close(callback);
-  },
-
-  missingStatus: function(test) {
-    test.expect(26);
-    http.request(this.options, function(res) {
-      common.checkResponse(test, res);
-      test.equals(res.statusCode, 409);
-      common.checkContent(test, res, function() {
-        test.ok(res.params);
-        test.ok(res.params.code);
-        test.ok(res.params.message);
-        test.equal(res.params.code, 'MissingParameter');
-        test.done();
-      });
-    }).end();
-  },
-
-  missingMetrics: function(test) {
-    test.expect(26);
-    this.options.path += '?status=ok';
-    http.request(this.options, function(res) {
-      common.checkResponse(test, res);
-      test.equals(res.statusCode, 409);
-      common.checkContent(test, res, function() {
-        test.ok(res.params);
-        test.ok(res.params.code);
-        test.ok(res.params.message);
-        test.equal(res.params.code, 'MissingParameter');
-        test.done();
-      });
-    }).end();
-  },
-
-  invalidStatus: function(test) {
-    test.expect(26);
-    this.options.path += '?status=' + uuid() + '&metrics=foo';
-    http.request(this.options, function(res) {
-      common.checkResponse(test, res);
-      test.equals(res.statusCode, 409);
-      common.checkContent(test, res, function() {
-        test.ok(res.params);
-        test.ok(res.params.code);
-        test.ok(res.params.message);
-        test.equal(res.params.code, 'InvalidArgument');
-        test.done();
-      });
-    }).end();
-  },
-
-  invalidMetricsNotObject: function(test) {
-    test.expect(26);
-    this.options.path += '?status=ok&metrics=foo';
-    http.request(this.options, function(res) {
-      common.checkResponse(test, res);
-      test.equals(res.statusCode, 409);
-      common.checkContent(test, res, function() {
-        test.ok(res.params);
-        test.ok(res.params.code);
-        test.ok(res.params.message);
-        test.equal(res.params.code, 'InvalidArgument');
-        test.done();
-      });
-    }).end();
-  },
-
-  invalidMetricsInvalidObject: function(test) {
-    test.expect(26);
-    this.options.path += '?status=ok';
-    this.options.headers['Content-Type'] = 'application/json';
-
-    var req = http.request(this.options, function(res) {
-      common.checkResponse(test, res);
-      test.equals(res.statusCode, 409);
-      common.checkContent(test, res, function() {
-        test.ok(res.params);
-        test.ok(res.params.code);
-        test.ok(res.params.message);
-        test.equal(res.params.code, 'InvalidArgument');
-        test.done();
-      });
+exports.test_missing_metrics = function(test, assert) {
+  http.request(_options('?status=ok'), function(res) {
+    common.checkResponse(assert, res);
+    assert.equal(res.statusCode, 409);
+    common.checkContent(assert, res, function() {
+      assert.ok(res.params);
+      assert.ok(res.params.code);
+      assert.ok(res.params.message);
+      assert.equal(res.params.code, 'MissingParameter');
+      test.finish();
     });
+  }).end();
+};
 
-    req.write(JSON.stringify({metrics: { foo: 'bar'} }));
-    req.end();
-  },
-
-  successWithObject: function(test) {
-    test.expect(14);
-    this.options.path += '?status=ok';
-    this.options.headers['Content-Type'] = 'application/json';
-
-    var req = http.request(this.options, function(res) {
-
-      common.checkResponse(test, res);
-      test.equals(res.statusCode, 202);
-      test.done();
+exports.test_invalid_status = function(test, assert) {
+  var opts = _options('?status=' + uuid() + '&metrics=foo');
+  http.request(opts, function(res) {
+    common.checkResponse(assert, res);
+    assert.equal(res.statusCode, 409);
+    common.checkContent(assert, res, function() {
+      assert.ok(res.params);
+      assert.ok(res.params.code);
+      assert.ok(res.params.message);
+      assert.equal(res.params.code, 'InvalidArgument');
+      test.finish();
     });
+  }).end();
+};
 
-    req.write(JSON.stringify({
-      metrics: {
-        name: 'urn:cpu:util',
-        type: 'Integer',
-        value: 95
-      }
-    }));
-    req.end();
-  },
-
-  successWithArray: function(test) {
-    test.expect(14);
-    this.options.path += '?status=ok';
-    this.options.headers['Content-Type'] = 'application/json';
-
-    var req = http.request(this.options, function(res) {
-
-      common.checkResponse(test, res);
-      test.equals(res.statusCode, 202);
-      test.done();
+exports.test_invalid_metrics_not_object = function(test, assert) {
+  http.request(_options('?status=ok&metrics=foo'), function(res) {
+    common.checkResponse(assert, res);
+    assert.equal(res.statusCode, 409);
+    common.checkContent(assert, res, function() {
+      assert.ok(res.params);
+      assert.ok(res.params.code);
+      assert.ok(res.params.message);
+      assert.equal(res.params.code, 'InvalidArgument');
+      test.finish();
     });
+  }).end();
+};
 
-    req.write(JSON.stringify({
-      metrics: [{
-        name: 'urn:cpu:util',
-        type: 'Integer',
-        value: 95
-      }]
-    }));
-    req.end();
-  }
+exports.test_invalid_metrics_invalid_object = function(test, assert) {
+  var req = http.request(_options('?status=ok'), function(res) {
+    common.checkResponse(assert, res);
+    assert.equal(res.statusCode, 409);
+    common.checkContent(assert, res, function() {
+      assert.ok(res.params);
+      assert.ok(res.params.code);
+      assert.ok(res.params.message);
+      assert.equal(res.params.code, 'InvalidArgument');
+      test.finish();
+    });
+  });
 
+  req.write(JSON.stringify({metrics: { foo: 'bar'} }));
+  req.end();
+};
 
-});
+exports.test_success_with_object = function(test, assert) {
+  var req = http.request(_options('?status=ok'), function(res) {
+    common.checkResponse(assert, res);
+    assert.equal(res.statusCode, 202);
+    test.finish();
+  });
+
+  req.write(JSON.stringify({
+    metrics: {
+      name: 'urn:cpu:util',
+      type: 'Integer',
+      value: 95
+    }
+  }));
+  req.end();
+};
+
+exports.test_success_with_array =  function(test, assert) {
+  var req = http.request(_options('?status=ok'), function(res) {
+    common.checkResponse(assert, res);
+    assert.equal(res.statusCode, 202);
+    test.finish();
+  });
+
+  req.write(JSON.stringify({
+    metrics: [{
+      name: 'urn:cpu:util',
+      type: 'Integer',
+      value: 95
+    }]
+  }));
+  req.end();
+};
+
+exports.tearDown = function(test, assert) {
+  app.close(function() {
+    test.finish();
+  });
+};
--- relay/tst/lib/common.js
+++ relay/tst/lib/common.js
@@ -5,31 +5,31 @@ var uuid = require('node-uuid');
 
 module.exports = {
 
-  checkResponse: function(test, response) {
-    test.ok(response);
-    test.ok(response.headers['access-control-allow-origin']);
-    test.ok(response.headers['access-control-allow-methods']);
-    test.ok(response.headers.server);
-    test.ok(response.headers.connection);
-    test.ok(response.headers.date);
-    test.ok(response.headers['x-api-version']);
-    test.ok(response.headers['x-request-id']);
-    test.ok(response.headers['x-response-time']);
+  checkResponse: function(assert, response) {
+    assert.ok(response);
+    assert.ok(response.headers['access-control-allow-origin']);
+    assert.ok(response.headers['access-control-allow-methods']);
+    assert.ok(response.headers.server);
+    assert.ok(response.headers.connection);
+    assert.ok(response.headers.date);
+    assert.ok(response.headers['x-api-version']);
+    assert.ok(response.headers['x-request-id']);
+    assert.ok(response.headers['x-response-time']);
 
-    test.equal(response.headers.server, 'Joyent');
-    test.equal(response.headers.connection, 'close');
-    test.equal(response.headers['x-api-version'], '6.1.0');
+    assert.equal(response.headers.server, 'Joyent');
+    assert.equal(response.headers.connection, 'close');
+    assert.equal(response.headers['x-api-version'], '6.1.0');
 
-    test.equal(response.httpVersion, '1.1');
+    assert.equal(response.httpVersion, '1.1');
   },
 
-  checkContent: function(test, response, callback) {
-    test.ok(response.headers['content-length']);
-    test.ok(response.headers['content-type']);
-    test.ok(response.headers['content-md5']);
+  checkContent: function(assert, response, callback) {
+    assert.ok(response.headers['content-length']);
+    assert.ok(response.headers['content-type']);
+    assert.ok(response.headers['content-md5']);
 
-    test.equal(response.headers['content-type'], 'application/json');
-    test.equal(response.headers.connection, 'close');
+    assert.equal(response.headers['content-type'], 'application/json');
+    assert.equal(response.headers.connection, 'close');
 
     response.setEncoding(encoding = 'utf8');
     response.body = '';
@@ -38,15 +38,15 @@ module.exports = {
     });
 
     response.on('end', function() {
-      test.equal(response.body.length, response.headers['content-length']);
+      assert.equal(response.body.length, response.headers['content-length']);
 
       var hash = crypto.createHash('md5');
       hash.update(response.body);
-      test.equal(hash.digest(encoding = 'base64'),
+      assert.equal(hash.digest(encoding = 'base64'),
                  response.headers['content-md5']);
 
       if (response.body.length > 0) {
-        test.doesNotThrow(function() {
+        assert.doesNotThrow(function() {
           response.params = JSON.parse(response.body);
         });
       }
