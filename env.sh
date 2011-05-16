b298223a73ca7146597a06d45344081fd6fbd33e
Trent Mick <trentm@gmail.com>
2011-05-16 11:25:26 -0700

    add 'bin/amon-mapi' convenience, add amon 'bin' dir to development env

--- env.sh
+++ env.sh
@@ -1,1 +1,2 @@
-export PATH=$PWD/deps/node-install/bin:$PWD/deps/redis/src:$PATH
+export AMON_MAPI_URL=http://localhost:8080
+export PATH=$PWD/deps/node-install/bin:$PWD/deps/redis/src:$PWD/bin:$PATH
