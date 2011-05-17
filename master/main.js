8f80f0021e4bb90232478c925580b9bd00db2425
Trent Mick <trentm@gmail.com>
2011-05-17 10:38:01 -0700

    Refactor. No functional change.

--- master/main.js
+++ master/main.js
@@ -1,25 +1,24 @@
-// Copyright 2011 Joyent, Inc.  All rights reserved.
+/*
+ * Copyright 2011 Joyent, Inc.  All rights reserved.
+ *
+ * Main entry-point for the Amon Master. The Amon master is the central
+ * API for managing monitor/check config, receiving events/alarms from agents
+ * and sending notifications.
+ */
+
 var path = require('path');
 var nopt = require('nopt');
 var log = require('restify').log;
 
 var amon_common = require('amon-common');
-
-var App = require('./lib/app');
-
 var Config = amon_common.Config;
 var Constants = amon_common.Constants;
 
+var App = require('./lib/app');
 
-// Global variable that holds a mapping of zone name to Apps
-var file = './cfg/amon-master.json';
-var port = 8080;
 
-function usage(code) {
-  console.log('usage: ' + path.basename(process.argv[1]) +
-              ' [-hd] [-f config_file] [-p port]');
-  process.exit(0);
-}
+
+//---- globals
 
 var opts = {
   'debug': Boolean,
@@ -34,27 +33,53 @@ var shortOpts = {
   'h': ['--help'],
   'p': ['--port']
 };
-var parsed = nopt(opts, shortOpts, process.argv, 2);
-if (parsed.help) usage(0);
-if (parsed.debug) log.level(log.Level.Debug);
-if (parsed.port) port = parsed.port;
-if (parsed.file) file = parsed.file;
 
-var cfg = new Config({
-  file: file
-});
-cfg.log = log;
-cfg.load(function(err) {
-  if (err) {
-    log.fatal('Unable to read config: ' + err);
-    process.exit(1);
+
+
+//---- internal support functions
+
+function usage(code, msg) {
+  if (msg) {
+    console.error('ERROR: ' + msg);
   }
-  cfg.plugins = require('amon-plugins');
-  var app = new App({
-    port: port,
-    config: cfg
+  console.log('usage: ' + path.basename(process.argv[1]) +
+              ' [-hd] [-f config_file] [-p port]');
+  process.exit(code);
+}
+
+
+//---- mainline
+
+function main() {
+  // Default config.
+  var file = './cfg/amon-master.json';
+  var port = 8080;
+
+  // Parser argv.
+  var parsed = nopt(opts, shortOpts, process.argv, 2);
+  if (parsed.help) usage(0);
+  if (parsed.debug) log.level(log.Level.Debug);
+  if (parsed.port) port = parsed.port;
+  if (parsed.file) file = parsed.file;
+
+  var cfg = new Config({
+    file: file
   });
-  app.listen(function() {
-    log.info('amon-master listening on ' + port);
+  cfg.log = log;
+  cfg.load(function(err) {
+    if (err) {
+      log.fatal('Unable to read config: ' + err);
+      process.exit(1);
+    }
+    cfg.plugins = require('amon-plugins');
+    var app = new App({
+      port: port,
+      config: cfg
+    });
+    app.listen(function() {
+      log.info('amon-master listening on ' + port);
+    });
   });
-});
+}
+
+main();
