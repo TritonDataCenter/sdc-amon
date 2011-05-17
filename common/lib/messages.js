55be50d78bea1a387e3c8af28917a5ebb39ab70c
Trent Mick <trentm@gmail.com>
2011-05-17 11:22:08 -0700

    document amon-common.Messages

--- common/lib/messages.js
+++ common/lib/messages.js
@@ -1,9 +1,24 @@
-// Copyright 2011 Joyent, Inc.  All rights reserved.
+/*
+ * Copyright 2011 Joyent, Inc.  All rights reserved.
+ *
+ * Amon common string messages. Could be useful for l10n.
+ *
+ * Usage:
+ *    var Messages = require('amon-common').Messages;
+ *
+ *    // Rendering a standard message.
+ *    var m = Messages.message(Messages.MissingParameter, "foo");
+ *
+ *    // Rendering a custom message.
+ *    var m = Messages.message("arg %s for zone %s is bogus: %o",
+ *                             argName, zone, errObject);
+ */
 
 var sprintf = require('sprintf').sprintf;
 
 module.exports = {
 
+  // Standard Amon messages.
   CustomerInvalidForCheck: 'customer %s is not associated with check %s',
   InvalidConfig: 'config is invalid: %s',
   InvalidMetric: 'metrics must contain an object with name, type and value',
