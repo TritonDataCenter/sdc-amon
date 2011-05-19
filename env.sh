7194dd861c10b7e71a5dd0a99c842ae6b1011e35
Trent Mick <trentm@gmail.com>
2011-05-19 15:42:19 -0700

    put bin dir on path

--- env.sh
+++ env.sh
@@ -7,5 +7,5 @@ fi
 
 ulimit -n 2048
 export AMON_MASTER=http://localhost:8080
-export PATH=$PWD/deps/node-install/bin:$PWD/deps/riak/rel/riak/bin:$PATH
+export PATH=$PWD/deps/node-install/bin:$PWD/bin:$PWD/deps/riak/rel/riak/bin:$PATH
 export MANPATH=$PWD/deps/riak/doc/man:$MANPATH
