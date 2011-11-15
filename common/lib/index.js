f81707fb1f1d035fd75202c6b965d18d3ca055cc
Trent Mick <trent.mick@joyent.com>
2011-11-15 11:09:25 -0800

    MON-12: drop old 'config' (now 'agentprobes') handler module: not used

--- common/lib/index.js
+++ common/lib/index.js
@@ -1,8 +1,6 @@
 // Copyright 2011 Joyent, Inc.  All rights reserved.
 
 module.exports = {
-
-  Config: require('./config'),
   Constants: require('./constants'),
   Messages: require('./messages'),
   Cache: require('./cache'),
