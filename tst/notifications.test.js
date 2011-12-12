89a24fce338fb3bf916dbfda69a73b14f6cf501d
Trent Mick <trent.mick@joyent.com>
2011-12-12 14:28:16 -0800

    MON-17: remove obsolete master/tst suite
    Add tst/notifications.test.js: remaining useful parts from
    master/tst/...

---
+++ tst/notifications.test.js
@@ -0,0 +1,65 @@
+var fs = require('fs');
+var test = require('tap').test;
+
+
+
+//---- globals
+
+var config;
+var notificationPlugins;
+var twilio;
+var email;
+
+
+//---- setup
+
+test('setup', function(t) {
+  fs.readFile(__dirname + '/config-notifications.json', 'utf8', function(err, content) {
+    t.notOk(err, err || '"config-notifications.json" loaded');
+    config = JSON.parse(content);
+    t.ok(config, "config parsed");
+    
+    notificationPlugins = {};
+    if (config.notificationPlugins) {
+      Object.keys(config.notificationPlugins || {}).forEach(function (name) {
+        var plugin = config.notificationPlugins[name];
+        var NotificationType = require(plugin.path);
+        notificationPlugins[name] = new NotificationType(plugin.config);
+      });
+    }
+    twilio = notificationPlugins.sms;
+    email = notificationPlugins.email;
+
+    t.end();
+  });
+});
+
+//---- test twilio
+
+test('twilio: sanitize empty', function(t) {
+  t.ok(!twilio.sanitizeAddress(null));
+  t.end();
+});
+
+test('twilio: sanitize NaN', function(t) {
+  t.ok(!twilio.sanitizeAddress('blah blah'));
+  t.end();
+});
+
+test('twilio: sanitize no spaces', function(t) {
+  t.equal(twilio.sanitizeAddress('2065665313'), '+12065665313');
+  t.end();
+});
+
+test('twilio: area code hyphens', function(t) {
+  t.equal(twilio.sanitizeAddress('206-566-5313'), '+12065665313');
+  t.end();
+});
+
+//---- test email
+
+test('email: sanitize empty', function(t) {
+  t.ok(!email.sanitizeAddress(null));
+  t.end();
+});
+
