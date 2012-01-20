0f1ad65d5ba2e61d9d30362f1a121044208e89b8
Trent Mick <trent.mick@joyent.com>
2012-01-19 21:46:52 -0800

    MON-33: turn on amon-agent and amon-relay in the GZ

--- agent/npm/postinstall.sh
+++ agent/npm/postinstall.sh
@@ -25,17 +25,14 @@ subfile "$DIR/../smf/amon-agent.smf.in" "$SMFDIR/amon-agent.xml"
 
 svccfg import $SMFDIR/amon-agent.xml
 
-## Gracefully restart the agent if it is online.
-#SL_STATUS=`svcs -H amon-agent | awk '{ print $1 }'`
-#echo "amon-agent status was $SL_STATUS"
-#if [ "$SL_STATUS" = 'online' ]; then
-#  svcadm restart amon-agent
-#else
-#  svcadm enable amon-agent
-#fi
-
-# Disabled by default until ready for prime time (MON-12).
-svcadm disable amon-agent
+# Gracefully restart the agent if it is online.
+SL_STATUS=`svcs -H amon-agent | awk '{ print $1 }'`
+echo "amon-agent status was $SL_STATUS"
+if [ "$SL_STATUS" = 'online' ]; then
+  svcadm restart amon-agent
+else
+  svcadm enable amon-agent
+fi
 
 # Ensure zero-exit value to not abort the npm install.
 exit 0
--- relay/npm/postinstall.sh
+++ relay/npm/postinstall.sh
@@ -37,20 +37,16 @@ subfile "$DIR/../smf/amon-relay.smf.in" "$SMFDIR/amon-relay.xml"
 svccfg import $SMFDIR/amon-relay.xml
 [[ -n "${ZWATCH_ENABLED}" ]] && svccfg import $SMFDIR/amon-zwatch.xml
 
-## Gracefully restart the agent if it is online.
-#SL_STATUS=`svcs -H amon-relay | awk '{ print $1 }'`
-#echo "Restarting amon-relay (status was $SL_STATUS)."
-#if [ "$SL_STATUS" = 'online' ]; then
-#  svcadm restart amon-relay
-#  [[ -n "${ZWATCH_ENABLED}" ]] && svcadm restart amon-zwatch
-#else
-#  svcadm enable amon-relay
-#  [[ -n "${ZWATCH_ENABLED}" ]] && svcadm enable amon-zwatch
-#fi
-
-# Disabled by default until ready for prime time (MON-12).
-svcadm disable amon-relay
-[[ -n "${ZWATCH_ENABLED}" ]] && svcadm disable amon-zwatch
+# Gracefully restart the agent if it is online.
+SL_STATUS=`svcs -H amon-relay | awk '{ print $1 }'`
+echo "Restarting amon-relay (status was $SL_STATUS)."
+if [ "$SL_STATUS" = 'online' ]; then
+  svcadm restart amon-relay
+  [[ -n "${ZWATCH_ENABLED}" ]] && svcadm restart amon-zwatch
+else
+  svcadm enable amon-relay
+  [[ -n "${ZWATCH_ENABLED}" ]] && svcadm enable amon-zwatch
+fi
 
 # Ensure zero-exit value to not abort the npm install.
 exit 0
