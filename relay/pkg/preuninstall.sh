d78d824cbbcf0760232a49a1150f6bc79389bd4c
Joshua M. Clulow <jmc@joyent.com>
2015-06-16 01:16:32 -0700

    AGENT-909 for now, only agents using "config-agent" should register with SAPI

--- relay/pkg/preuninstall.sh
+++ relay/pkg/preuninstall.sh
@@ -9,6 +9,11 @@
 # Copyright (c) 2014, Joyent, Inc.
 #
 
+if [[ "${SDC_AGENT_SKIP_LIFECYCLE:-no}" = "yes" ]]; then
+  printf 'Running during package build; skipping lifecycle script.\n' >&2
+  exit 0
+fi
+
 OS=$(uname -s)
 
 if [[ $OS != "SunOS" ]]; then
